b561943a831dad5a434ba179883fcb0110bdf1e5
huongnt.iteal@gmail.com <huongnt.iteal@gmail.com>
2021-03-26 22:52:45 +0700

    test

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        const token = req.headers.authorization.split('Bearer')[1];
         jwt.verify(token, 'secret_this_should_be_longer');
         next();
     } catch (error) {
         res.status(401).json({
-            message: 'Auth failed!'
-        });
+            message: 'Auth failed'
+        })
     }
+   
 }
