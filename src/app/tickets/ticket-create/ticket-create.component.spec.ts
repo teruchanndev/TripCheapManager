e07224f39f664ae4837b892ca526629565d9c801
huongnt.iteal@gmail.com <huongnt.iteal@gmail.com>
2021-04-01 11:52:22 +0700

    update 11

--- src/app/tickets/ticket-create/ticket-create.component.spec.ts
+++ src/app/tickets/ticket-create/ticket-create.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TicketCreateComponent } from './ticket-create.component';
 
@@ -6,7 +6,7 @@ describe('TicketCreateComponent', () => {
   let component: TicketCreateComponent;
   let fixture: ComponentFixture<TicketCreateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TicketCreateComponent ]
     })
--- src/app/tickets/ticket-edit/ticket-edit.component.spec.ts
+++ src/app/tickets/ticket-edit/ticket-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TicketEditComponent } from './ticket-edit.component';
 
@@ -6,7 +6,7 @@ describe('TicketEditComponent', () => {
   let component: TicketEditComponent;
   let fixture: ComponentFixture<TicketEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TicketEditComponent ]
     })
